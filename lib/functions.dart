6014d47ba93ed1c3a4b6d6f1183fa8f3bb847006
ManarShahin48 <manarshahin48@gmail.com>
2022-12-31 12:38:09 +0200

    Finish the second file process

--- lib/functions.dart
+++ lib/functions.dart
@@ -48,10 +48,10 @@ Future<List<List<dynamic>>> createFirstFile() async {
 }
 
 Future<List<List<dynamic>>> createSecondFile() async {
-  int? ordersNum = csvData?.length;
-
   Map<String, List<String>> orderNum = {};
-  print(csvData);
+  Map<String, String> popularVal = {};
+  List<List<dynamic>> secondData = [];
+
   for (List<dynamic> val in csvData!) {
     String key = val[2];
     String value = val[4];
@@ -63,20 +63,44 @@ Future<List<List<dynamic>>> createSecondFile() async {
   }
   print(orderNum);
 
-  List<List<dynamic>> secondData = [];
   List<String> keys = orderNum.keys.toList();
-  List<List<String>> values = orderNum.values.toList();
-  // for (int i = 0; i < keys.length; i++) {
-  //   String key = keys[i];
-  //   String? value = orderNum[key] as String?;
-  //   secondData.add([key, value]);
-  // }
 
+  for (int i = 0; i < keys.length; i++) {
+    String key = keys[i];
+    List<String>? value = orderNum[key];
+
+    Map<String, int> frequency = {};
+    for (String string in value!) {
+      if (frequency.containsKey(string)) {
+        frequency[string] = frequency[string]! + 1;
+      } else {
+        frequency[string] = 1;
+      }
+    }
+
+    int? maxVal = 0;
+    String newKey = '';
+    for (String key in frequency.keys) {
+      if (frequency[key]! > maxVal!) {
+        newKey = key;
+        maxVal = frequency[key];
+      }
+    }
+
+    popularVal[key] = newKey;
+  }
+
+  print(popularVal);
+
+  for (int i = 0; i < keys.length; i++) {
+    String key = keys[i];
+    String? value = popularVal[key];
+    secondData.add([key, value]);
+  }
   Directory dir = await getApplicationDocumentsDirectory();
   File file = File('${dir.path}/secondDataFile.csv');
   String secondDataFile = const ListToCsvConverter().convert(secondData);
-  print(values);
-  // print(secondDataFile);
+  print(secondDataFile);
   await file.writeAsString(secondDataFile);
   return const CsvToListConverter().convert(secondDataFile, eol: "\n");
 }
